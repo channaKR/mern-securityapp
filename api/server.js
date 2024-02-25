13a22896a07156e80f1ea721f6f8fdfe6a13af06
channaKR <channar.official@gmail.com>
2024-02-25 10:22:38 +0530

    Add mongoose

--- api/server.js
+++ api/server.js
@@ -1,7 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("Hello Port 3000")
+app.listen(process.env.PORT,()=>{
+    console.log(`PORT ${process.env.PORT}`)
 })
+
+mongoose.connect(process.env.mongo)
+.then(()=>
+    {
+        console.log("Conected to the database")
+    }
+
+)
+.catch((error)=>
+    {
+        console.log(error)
+    }
+)
