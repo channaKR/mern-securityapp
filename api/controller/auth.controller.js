6c8ff6a1cfe3f07e645a7896c7ebfa05d2be3ec3
channaKR <channar.official@gmail.com>
2024-03-10 22:44:37 +0530

    Add try catch

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -4,10 +4,17 @@ export const signUp = async(req,res)=>{
 
     const { username,email,password } = req.body;
     const newUser = new User({username,email,password});
-    await newUser.save();
-    res.status(201).json({
-        message:"User Added to the Db"
-    })
+    try{
+        await newUser.save();
+        res.status(201).json({
+            message:"User Added to the Db"
+        });
+    }catch(error){
+
+        res.status(500).json({
+            message:error.message
+        })
+    }
 
    
 }
